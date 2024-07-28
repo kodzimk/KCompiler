8661421180825ce0349f71b792c30e7cf4ba94ef
kodzimk <proggamedev@gmail.com>
2024-07-28 10:52:02 +0500

    first syntax for our language and compiler returning value

--- KCompiler/src/main.cpp
+++ KCompiler/src/main.cpp
@@ -80,6 +80,31 @@ std::vector<Token> tokenize(const std::string& str) {
     return tokens;
 }
 
+std::string tokens_to_asm(const std::vector<Token>& tokens)
+{
+    std::stringstream output;
+    output << "global _start\n_start:\n";
+
+    for (int i = 0;i<tokens.size();i++)
+    {
+        const Token& token = tokens.at(i);
+
+        if (token.type == TokenType::_return) 
+        {
+            if (i + 1 < tokens.size() && tokens.at(i + 1).type == TokenType::int_lit)
+            {
+               if (i + 2 < tokens.size() && tokens.at(i + 2).type == TokenType::semi) 
+               {
+                   output << "   mov rax,60\n";
+                   output << "   mov rdi," << tokens.at(i + 1).value.value()<<"\n";
+                   output << "   syscall";
+               }
+            }
+        }
+    }
+    return output.str();
+}
+
 int main(int argc, char* argv[]) {
 
     std::string contents;
@@ -92,6 +117,14 @@ int main(int argc, char* argv[]) {
     }
 
     std::vector<Token> tokens = tokenize(contents);
-    std::cout << tokens.size();
+
+    {
+        std::fstream file("out.asm", std::ios::out);
+        file << tokens_to_asm(tokens);
+    }
+
+    system("nasm -felf64 out.asm");
+    system("ld -o out out.o");
+
     return EXIT_SUCCESS;
 }
