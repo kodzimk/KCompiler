15e31f31a5afa9773c21fd292e66ca15c51341c0
kodzimk <proggamedev@gmail.com>
2024-08-03 15:00:37 +0500

    o my dod linux ubuntu and i can execute

--- KCompiler/out.asm
+++ KCompiler/out.asm
@@ -2,25 +2,25 @@ global _start
 _start:
     mov rax, 2
     push rax
-    mov rax, 1
+    mov rax, 3
     push rax
-    pop rax
-    pop rbx
-    div rbx
-    push rax
-    mov rax, 4
-    push rax
-    mov rax, 5
+    mov rax, 2
     push rax
     pop rax
     pop rbx
     mul rbx
     push rax
+    mov rax, 10
+    push rax
     pop rax
     pop rbx
-    add rax, rbx
+    sub rax, rbx
     push rax
-    mov rax, 8
+    pop rax
+    pop rbx
+    div rbx
+    push rax
+    mov rax, 1
     push rax
     mov rax, 1
     push rax
@@ -54,10 +54,6 @@ label2:
     syscall
     add rsp, 0
 label1:
-    push QWORD [rsp + 8]
-    mov rax, 60
-    pop rdi
-    syscall
     mov rax, 60
     mov rdi, 0
     syscall
---
+++ KCompiler/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+    echo "The name of first fruit is"
