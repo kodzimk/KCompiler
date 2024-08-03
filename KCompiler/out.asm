a9b379ebca0184eaca8262f9b0a293f934103abc
kodzimk <proggamedev@gmail.com>
2024-08-03 15:12:22 +0500

    some testing

--- KCompiler/out.asm
+++ KCompiler/out.asm
@@ -2,6 +2,16 @@ global _start
 _start:
     mov rax, 2
     push rax
+    mov rax, 5
+    push rax
+    pop rax
+    pop rbx
+    mul rbx
+    push rax
+    mov rax, 2
+    push rax
+    mov rax, 2
+    push rax
     mov rax, 3
     push rax
     mov rax, 2
@@ -20,10 +30,17 @@ _start:
     pop rbx
     div rbx
     push rax
+    pop rax
+    pop rbx
+    div rbx
+    push rax
+    pop rax
+    pop rbx
+    add rax, rbx
+    push rax
     mov rax, 1
     push rax
-    mov rax, 4
-    push rax
+    push QWORD [rsp + 8]
     mov rax, 60
     pop rdi
     syscall
